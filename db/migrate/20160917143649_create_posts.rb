0438883a39b7e98a06c4035b77a95fd7c575aa5e
Mykyta Troianov <nicktroyanov@gmail.com>
2016-09-17 22:50:42 +0300

    modified sign in, added likes model

--- db/migrate/20160917143649_create_posts.rb
+++ db/migrate/20160917143649_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.references :user, foreign_key: true
       t.text :text
-      t.integer :likes_count
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917143649) do
+ActiveRecord::Schema.define(version: 20160917194831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "text"
-    t.integer  "likes_count"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 20160917143649) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
